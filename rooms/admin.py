fbe59e1fde5b89444e83852a70a3bf5869da52c1
손겸 <sonny@songyeom-ui-MacBookAir.local>
2022-03-21 12:15:25 +0900

    Complete Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,16 +4,80 @@ from . import models
 
 @admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
     """Item Admin Definition"""
+
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
     """Room Admin Definition"""
 
-    pass
+    # 관리자 페이지 외부 리스트
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    # 관리자 페이지 내부
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(models.Photo)
