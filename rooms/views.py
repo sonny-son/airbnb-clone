7d7ea7aee37548be1c40100a0b93f86adf81d834
Kyeom <son_6631@naver.com>
2022-03-30 20:38:29 +0900

    Complete page navigation Paginator Version

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,15 @@
 from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page_size = 10
-    limit = page * page_size
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    page = request.GET.get("page", 1)  # url 에서 key : page인 정보를 가져온다.
+    room_list = models.Room.objects.all()  # 리스트를 만든다.
+    paginator = Paginator(room_list, 10, orphans=5)  # paginator은 모든 방과 관련된 정보를 가지고 있음
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
