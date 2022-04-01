18c16d94cc28409553f11ff2251bf2ace075082b
Kyeom <son_6631@naver.com>
2022-04-01 21:41:33 +0900

    Room Detail CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -14,9 +12,8 @@ class HomeView(ListView):
     ordering = "created"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    """RoomDetail Definition"""
+
+    model = models.Room
