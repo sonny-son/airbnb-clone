8c1857a7a4ee88e08f8e56fda76997ae70843939
Kyeom <son_6631@naver.com>
2022-04-17 23:24:42 +0900

    Login by FBV

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
@@ -6,3 +7,15 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     # passwordinput 비밀번호가 안보이게 가리게함
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User does not exist"))
--- users/views.py
+++ users/views.py
@@ -1,13 +1,26 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "sonny@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+        return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
