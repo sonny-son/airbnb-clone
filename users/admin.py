1dc455ba3b6af408d0a9ccf2ee580467113b3f26
Kyeom <son_6631@naver.com>
2022-04-19 11:44:19 +0900

    Complete Github Login

--- users/admin.py
+++ users/admin.py
@@ -34,6 +34,7 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
 
     # 관리자 페이지 내부
@@ -51,6 +52,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
